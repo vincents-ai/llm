2125b6189eaabf95f97e0b8c34e0b42b1bafd968
Vincent Palmer <shift@someone.section.me>
2026-01-23 18:54:03 +0100

    feat: control API stubs for configure, reset, log endpoints with TDD tests; update flake.nix devShell structure for Nix compatibility [universal-llm-mock-server]

--- flake.nix
+++ flake.nix
@@ -9,17 +9,21 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; overlays = [ (import rust-overlay) ]; };
     in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.rust-bin.stable.latest.default
-          pkgs.openssl
-          pkgs.pkg-config
-          pkgs.cacert
-        ];
-        RUST_BACKTRACE = "1";
-        shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH
-        '';
+      devShells = {
+        x86_64-linux = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.rust-bin.stable.latest.default
+              pkgs.openssl
+              pkgs.pkg-config
+              pkgs.cacert
+            ];
+            RUST_BACKTRACE = "1";
+            shellHook = ''
+              export LD_LIBRARY_PATH=${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH
+            '';
+          };
+        };
       };
     };
 }
